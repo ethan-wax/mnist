de9d0557d033d2decb6283767b84307e9cac564a
Ethan Waxman <ejw99@cornell.edu>
2025-06-22 11:46:24 -0400

    Create simple MLP model

---
+++ model/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_size=784, hidden_size=100, num_classes=10):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(input_size, hidden_size),
+            nn.Sigmoid(),
+            nn.Linear(hidden_size, num_classes),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
